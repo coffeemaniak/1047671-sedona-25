7bbd4c787200a4d4cc4e4e33ff0466c5e5849e07
coffeemaniak <50338957+coffeemaniak@users.noreply.github.com>
2019-06-23 10:16:26 +0300

    проект для защиты

--- js/script.js
+++ js/script.js
@@ -3,16 +3,22 @@
    var from = form.querySelector("[name=date-from]");
    var to = form.querySelector("[name=date-to]");
    var result = form.querySelector(".result-form");
+   // var calendar = form.querySelector(".calendar");
+   // var minus = form.querySelector(".minus");
+   // var plus = form.querySelector(".plus");
    link.addEventListener("click", function (evt) {
     evt.preventDefault();
     //alert("hi");
     form.classList.toggle("form-animation");
     from.focus();
   });
-  form.addEventListener("submit", function (evt) {
-    if (!from.value || !to.value) {
-      evt.preventDefault();
-      console.log("Нужно указать дату заезда и дату выезда");
-      result.classList.toggle("show-result");
-    }
-    });
+   form.addEventListener("submit", function (evt) {
+     if (!from.value || !to.value) {
+       evt.preventDefault();
+       console.log("Нужно указать дату заезда и дату выезда");
+       result.classList.toggle("show-result");
+     }
+     });
+     // calendar.addEventListener("submit", function (evt) {
+       // evt.preventDefault();
+     // })
